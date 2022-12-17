78de5ac01352d55d64318871d3207aa01430080f
Sam B <reposme@outlook.com>
2022-12-17 09:01:44 +0000

    Appended h2 element to the id #today & added placeholder text ready for appending a moment.js object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 // console.log("Hello");
 
-//Create a variable to target the id #today
-var todayDate = $('#today');
+//Create a variable to get the id #today
+var todayDate = $("#today");
 
 // console.log(todayDate);
 
-//Create a variable that will hold a "h2" element
-var cityTitleEl = $('<h2>');
+//Create a variable that will create a "h2" element
+var cityTitleEl = $("<h2>"+ "Today's Date" + "</h2>");
 
+
+//Append h2 element to id #today
+
+todayDate.append(cityTitleEl);
