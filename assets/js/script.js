bf1c374927bdb9d3719d1b269fee3532ae263c23
Sam B <reposme@outlook.com>
2022-12-17 08:54:44 +0000

    Created a variable to get id #today & a variable to create a h2 element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
-#cityData {
-    border-style: solid;
-}
+// console.log("Hello");
+
+//Create a variable to target the id #today
+var todayDate = $('#today');
+
+// console.log(todayDate);
+
+//Create a variable that will hold a "h2" element
+var cityTitleEl = $('<h2>');
+
