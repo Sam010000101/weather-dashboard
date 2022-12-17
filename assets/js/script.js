3ea6739ba5d4887cff396a8c42c086d30b02e504
Sam B <reposme@outlook.com>
2022-12-17 09:39:50 +0000

    Created cityTemp var to hold city temperature and appended it to the id #today

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,14 @@ var cityTitleEl = $("<h2>"+ "City " + "(" + todaysDate.format("D/MM/YYYY") + ") 
 //Append h2 element to id #today
 todayEl.append(cityTitleEl);
 
+//Variable to create a "h5" element that will hold the city temperature
+var cityTemp = $("<h5>" + "Temp:" + "</h5>");
+
+//Append cityTemp var to id #today
+todayEl.append(cityTemp);
+
+
+// var cityWindSpeed = "Wind:";
+// var cityHumidity = "Humidity:";
+
+console.log(cityTemp);
