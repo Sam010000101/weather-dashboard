06f66603a55b965f1549e0d101cfb2cdfd1e7a28
Sam B <reposme@outlook.com>
2022-12-18 21:31:36 +0000

    Added handleFormSubmit function to log city & prevent default behaviour

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,7 @@ var cityTemp = $("<h5>" + "Temp: " + "</h5>");
 
 //Append cityTemp var to id #today
 todayEl.append(cityTemp);
+// console.log(cityTemp);
 
 //Variable to create a "h5" element that will hold the city wind speed
 var cityWindSpeed = $("<h5>" + "Wind: " + "</h5>");
@@ -30,24 +31,43 @@ var cityHumidity = $("<h5>" + "Humidity: " + "</h5>");
 //Append cityHumidity var to id #today
 todayEl.append(cityHumidity);
 
+//Variables that target the search form & search input using the respective ids
+var formEl = $("#search-form");
+var citySearch = $("#search-input");
 
-console.log(cityTemp);
+function handleFormSubmit(event) {
+    //prevent the default behavior
+    event.preventDefault();
 
-//Variables to hold geographical location
-var longitude = "44.1";
-var latitude = "44.1";
-var coordinates = "lat=" + latitude + "&" + "lon=" + longitude;
+    console.log(citySearch.val());
+
+    //Clear input fields
+    $('input[type="text"]').val("");
+}
+
+//Submit event on the form
+formEl.on("submit", handleFormSubmit);
 
 
-//Variable to hold api query url
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + coordinates + "&appid=f51bb6b0db1117a9e5526eaa8621c68c";
 
-//Perform an asynchronous HTTP (Ajax) request
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-// console.log(coordinates);
+// //Variables to hold geographical location
+// var longitude = "44.1";
+// var latitude = "44.1";
+// var coordinates = "lat=" + latitude + "&" + "lon=" + longitude;
+
+
+// //Variable to hold api query url
+// var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + coordinates + "&appid=f51bb6b0db1117a9e5526eaa8621c68c";
+
+// //Perform an asynchronous HTTP (Ajax) request
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+// // console.log(coordinates);
+
+// // //logs the temperature of the first array item
+// // console.log(response.list[0].main.temp);
   
-});
+// });
