4301c6a4bc7dc6062b125b4365613b6d0a6c7e1c
Sam B <reposme@outlook.com>
2022-12-19 08:29:42 +0000

    Used JQuery to append api city weather response JSON to current weather (id #today)

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,58 @@ var todaysDate = moment();
 //Variable to get the id #today
 var todayEl = $("#today");
 
-//Variable to get the buttons by id
+// //Variable to create a "h5" element that will hold the city temperature
+// var cityTemp = $("<h5>" + "Temp: " + "</h5>");
 
 
+// //Variable to create a "h5" element that will hold the city wind speed
+// var cityWindSpeed = $("<h5>" + "Wind: " + "</h5>");
+
+// //Append cityWindSpeed var to id #today
+// todayEl.append(cityWindSpeed);
+
+// //Variable to create a "h5" element that will hold the city humidity
+// var cityHumidity = $("<h5>" + "Humidity: " + "</h5>");
+
+// //Append cityHumidity var to id #today
+// todayEl.append(cityHumidity);
+
+// //Variables to hold geographical location
+// var longitude = "44.1";
+// var latitude = "44.1";
+// var coordinates = "lat=" + latitude + "&" + "lon=" + longitude;
+
+var city = "London";
+
+//Variable to hold api query url
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f51bb6b0db1117a9e5526eaa8621c68c";
+
+//Perform an asynchronous HTTP (Ajax) request
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    // console.log(response.main.temp);
+    
+// //logs the temperature of the first array item
+// console.log(response.list[0].main.temp);
+ 
+//Convert Kelvin to degrees c
+
+// [insert code here]
+
+//Append cityTemp var to id #today & add temperature data from OpenWeather API
+
+$(todayEl).append("<h5>" + "Temp: " + response.main.temp + " \u00B0C" + "</h5>");
+$(todayEl).append("<h5>" + "Wind Speed: " + response.wind.speed + " KPH" + "</h5>");
+$(todayEl).append("<h5>" + "Humidity: " + response.main.humidity + "%" + "</h5>");
 
 
+});
 
-//Variable to create a "h5" element that will hold the city temperature
-var cityTemp = $("<h5>" + "Temp: " + "</h5>");
 
-//Append cityTemp var to id #today
-todayEl.append(cityTemp);
-// console.log(cityTemp);
-
-//Variable to create a "h5" element that will hold the city wind speed
-var cityWindSpeed = $("<h5>" + "Wind: " + "</h5>");
-
-//Append cityWindSpeed var to id #today
-todayEl.append(cityWindSpeed);
-
-//Variable to create a "h5" element that will hold the city humidity
-var cityHumidity = $("<h5>" + "Humidity: " + "</h5>");
-
-//Append cityHumidity var to id #today
-todayEl.append(cityHumidity);
 
 //Prevent default on Search input (Form Elements)//
 //--------------------------------//
@@ -66,6 +94,7 @@ function handleFormSubmit(event) {
     historyButtonFive.html("<p>" + (citySearch.val()) + "</p>");
     historyButtonSix.html("<p>" + (citySearch.val()) + "</p>");
     
+    //Stops the prepends from 'stacking up' when the search button is clicked multiple times
    $(this).off (event);
 
     //Clear input fields
@@ -79,25 +108,5 @@ formEl.on("submit", handleFormSubmit);
 
 
 
-// //Variables to hold geographical location
-// var longitude = "44.1";
-// var latitude = "44.1";
-// var coordinates = "lat=" + latitude + "&" + "lon=" + longitude;
 
-
-// //Variable to hold api query url
-// var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + coordinates + "&appid=f51bb6b0db1117a9e5526eaa8621c68c";
-
-// //Perform an asynchronous HTTP (Ajax) request
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response);
-// // console.log(coordinates);
-
-// // //logs the temperature of the first array item
-// // console.log(response.list[0].main.temp);
-  
-// });
 
